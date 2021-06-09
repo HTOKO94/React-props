28264f746997b6e99d5acec17f27c011cae0a332
HTOKO94 <hamza.toukabri94@gmail.com>
2021-06-09 19:28:08 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import P from "./Components/Profile/Profile";
+import './Components/Profile/Profile.css';
+import A from "./Components/About/About";
+import "./Components/About/About.css";
 
 function App() {
+   const handleName  = fullname => alert(fullname);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
+     <div className="aboutme">
+     { <P  fullname="Hamza Toukebri" bio="Full Stack Developer JS" profession="Web developer"  handleName={handleName}>Hamza.jpg</P> }
+     <A fullname="Hamza Toukebri" location="Beja,Tunisia" degree="BAC+3" phone="55320994" email="hamza.toukabri94@gmail.com" dateOfBirth="02/09/1994" experience="1 Year" careerLevel="First-Level"/>
+     </div>
     </div>
   );
 }
+
+
 
 export default App;
